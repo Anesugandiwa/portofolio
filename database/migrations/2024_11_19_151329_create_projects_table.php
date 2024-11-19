ca50bdbfda220ab9aa2c5c3fc04370de7b339a54
Anesu Gandiwa <anesugandiwa9@gmail.com>
2024-11-19 18:22:04 +0200

    first commit

---
+++ database/migrations/2024_11_19_151329_create_projects_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('name_of_project');
+            $table->text('description');
+            $table->string('technologies_used')->nullable();
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->string('project_url')->nullable();
+            $table->string('github_url')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('projects');
+    }
+};
